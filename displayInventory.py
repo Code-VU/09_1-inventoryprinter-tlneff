a4c11d506d3c1ee7ce72b30a27d5952f7b817234
tlneff <neff.tyler@gmail.com>
2024-04-19 22:26:40 -0500

    added program logic

--- displayInventory.py
+++ displayInventory.py
@@ -1,7 +1,12 @@
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
+
 def displayInventory(inventory):
     # your code goes here
+    print("Inventory:")
+    for item, quantity in inventory.items():
+        print(quantity, item)
+    print("Total number of items:", sum(inventory.values()))
 
 
 if __name__ == "__main__":
